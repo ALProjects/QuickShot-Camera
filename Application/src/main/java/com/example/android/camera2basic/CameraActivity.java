0e44414649ce08efd2644ee9458e55d41bc4dea1
ALProjects <Aaron@Macbook.local>
2017-09-03 11:49:45 -0700

    Clean up
    Cleaned up code.

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -30,7 +30,6 @@ public class CameraActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-
         if (null == savedInstanceState) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.container, Camera2BasicFragment.newInstance())
